245b33b857feddfcc63789840239766f07eb82f9
ByDageta <119523917+ByDageta@users.noreply.github.com>
2024-03-18 11:04:57 +0100

    Update main.c
    Épico

--- main.c
+++ main.c
@@ -1,98 +1,113 @@
 #include <stdio.h>
 
-//Tendero Beltrán, Antonio, 49428050S
-//García Guirado, David
-//García Marín, Jorge
+/*
+    GREEDY ALGORITHMS ASSIGNMENT
+    - Tendero Beltrán, Antonio (49428050S)
+    - García Guirado, David (55052860Z)
+    - García Marín, Jorge (49314477Q)
+*/
 
 
-// Swap 2 elements of the Array
-void swap(int *x, int *y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
+// Declaration of functions (SORTING ALGORITHM)
+void mergeSort(int arr[], int l, int r);
+void merge(int arr[], int l, int m, int r);
 
-// Partition function
-int partition(int arr[], int low, int high) {
-    // initialize pivot to be the first element
-    int pivot = arr[low];
-    int i = low;
-    int j = high;
-
-    while (i < j) {
-        // condition 1: find the first element greater than
-        // the pivot (from starting)
-        while (arr[i] <= pivot && i <= high - 1) {
-            i++;
-        }
-        // condition 2: find the first element smaller than
-        // the pivot (from last)
-        while (arr[j] > pivot && j >= low + 1) {
-            j--;
-        }
-        if (i < j) {
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[low], &arr[j]);
-    return j;
-}
-
-// QuickSort function
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        // call Partition function to find Partition Index
-        int partitionIndex = partition(arr, low, high);
-
-        // Recursion to left and right
-        quickSort(arr, low, partitionIndex - 1);
-        quickSort(arr, partitionIndex + 1, high);
-    }
-}
 
 // GREEDY ALGORITHM
 int greedy(int weight[], int n, int totalWeight, int selected[]) {
-    int j = 0, money = 0;
-    quickSort(weight, 0, n); // SORT
+    int money = 0, currentWeight = 0;
+    if (n > 1) mergeSort(weight, 0, n-1); // SORT (only when more than 1 element)
 
     for (int i = 0; i < n; ++i) {
-        if (weight[i] <= totalWeight) { // Check if another item stills fits
-            selected[j] = weight[i]; // Include the weight of the selected item
-            j++;
+        if (currentWeight + weight[i] <= totalWeight) { // Check if another item stills fits
+            selected[i] = weight[i]; // Include the weight of the selected item
             money += 40; // Increment money
-            totalWeight -= weight[i]; // Subtract the weight from the total weight left
+            currentWeight += weight[i]; // Subtract the weight from the total weight left
         } else break;
     }
     return money; // Return total money and the array of selected items' weight
 }
 
 void main() {
-    int list[20];
+    int list[20]; // CANDIDATES (up to 20)
     int i = 0, input = 0;
 
+    // Ask for the input (Candidates)
     printf("Enter weight for each item:\n");
-    while (input != -1) {
+    while (1) {
         scanf("%d", &input);
         if (input == -1) break;
         list[i] = input;
         i++;
     }
 
-    printf("\nSorted list:\n");
-    for (int j = 0; j < i; ++j) { // Print list sorted
-        printf("%d\n", list[j]);
-    }
-
-
-    // GREEDY ALGORITHM
+    // GREEDY ALGORITHM >>> Store selected elements in array 'selected'
     int selected[i];
     int money = greedy(list, i, 20, selected);
 
-    // Print results
+    // Print results = Money + Selected items
     printf("Money = %d\n", money);
-    printf("Selected items' weight:\n");
-    for (int j = 0; j < i; ++j) { // Print list sorted
-        printf("%d, ", list[j]);
+    printf("Selected items' (%d) weight:\n", money/40);
+    for (int j = 0; j < money/40; ++j) { // Print list sorted (money/40 = Nº elements)
+        printf("%d, ", selected[j]);
+    }
+}
 
+
+// MERGE SORT ALGORITHM
+// l = left-most index of the array | r = right-most index of the array
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) { // Only if more than 1 element OR right arguments (l < r)
+        int m = l + (r - l) / 2; // Index in the middle of the array
+
+        // Sort 1st and 2nd halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the already sorted halves
+        merge(arr, l, m, r);
+    }
+}
+
+// Merges the 2 sub-arrays of arr[]
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1; // Length of the 1st array = arr[l..m]
+    int n2 = r - m; // Length of the 2nd array = arr[m+1..r]
+
+    // Create temp arrays AND initialize with argument data
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) { // Include element from the left if smaller
+            arr[k] = L[i];
+            i++;
+        }
+        else { // Include element from the right if bigger
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[] (if any)
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[] (if any)
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
